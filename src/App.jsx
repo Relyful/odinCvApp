52710ff151a42d698457b4012992a2b392e839ab
Relyful <13381270+Relyful@users.noreply.github.com>
2024-10-10 21:16:38 +0200

    Create handle functions for all inputs and display values of inputs on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,103 @@
-import { useState } from 'react'
-import './App.css'
-import ModalForm from './components/cvForm'
+import { useState } from "react";
+import "./App.css";
+import ModalForm from "./components/cvForm";
 
 function App() {
   const [dialogOpen, setDialogOpen] = useState(true);
-  const [name, setName] = useState('John Doe');
-  const [email, setEmail] = useState('john.doe@gmail.com');
-  const [phone, setPhone] = useState('+420 123 456');
-  const [schoolName, setSchoolName] = useState('School of Internet');
-  const [schoolTitle, setSchoolTitle] = useState('Mechanic of Computer Networks');
-  const [schoolYear, setSchoolYear] = useState('2009');
-  const [jobName, setJobName] = useState('Random Name Security');
-  const [jobTitle, setJobTitle] = useState('Guard');
-  const [jobResponsibility, setJobResponsibility] = useState('Guarding');
-  const [jobStartDate, setJobStartDate] = useState(new Date(2014, 7, 1));
+  const [name, setName] = useState("John Doe");
+  const [email, setEmail] = useState("john.doe@gmail.com");
+  const [phone, setPhone] = useState("+420 123 456");
+  const [schoolName, setSchoolName] = useState("School of Internet");
+  const [schoolTitle, setSchoolTitle] = useState(
+    "Mechanic of Computer Networks"
+  );
+  const [schoolYear, setSchoolYear] = useState("2009");
+  const [jobName, setJobName] = useState("Random Name Security");
+  const [jobTitle, setJobTitle] = useState("Guard");
+  const [jobResponsibility, setJobResponsibility] = useState("Guarding");
+  const [jobStartDate, setJobStartDate] = useState(new Date(2014, 6, 1));
 
   const handleClose = () => {
     setDialogOpen(false);
-  }
+  };
 
   const handleOpen = () => {
     setDialogOpen(true);
-  }
+  };
+
+  const handleName = (name) => {
+    setName(name);
+  };
+
+  const handleEmail = (email) => {
+    setEmail(email);
+  };
+
+  const handlePhone = (phone) => {
+    setPhone(phone);
+  };
+
+  const handleSchoolName = (schoolName) => {
+    setSchoolName(schoolName);
+  };
+
+  const handleSchoolTitle = (schoolTitle) => {
+    setSchoolTitle(schoolTitle);
+  };
+
+  const handleSchoolYear = (schoolYear) => {
+    setSchoolYear(schoolYear);
+  };
+
+  const handleJobName = (jobName) => {
+    setJobName(jobName);
+  };
+
+  const handleJobTitle = (jobTitle) => {
+    setJobTitle(jobTitle);
+  };
+
+  const handleJobRespo = (jobRespo) => {
+    setJobResponsibility(jobRespo);
+  };
+
+  const handleJobStartDate = (jobStartDate) => {
+    setJobStartDate(jobStartDate);
+  };
 
   return (
     <>
-      <ModalForm  onClose={handleClose} dialogOpen={dialogOpen} />
-      <button type="button" onClick={handleOpen}>Open Form</button>
+      <ModalForm
+        onClose={handleClose}
+        dialogOpen={dialogOpen}
+        handleName={handleName}
+        handleEmail={handleEmail}
+        handlePhone={handlePhone}
+        handleSchoolName={handleSchoolName}
+        handleSchoolTitle={handleSchoolTitle}
+        handleSchoolYear={handleSchoolYear}
+        handleJobName={handleJobName}
+        handleJobTitle={handleJobTitle}
+        handleJobRespo={handleJobRespo}
+        handleJobStartDate={handleJobStartDate}
+      />
+      <button type="button" onClick={handleOpen}>
+        Open Form
+      </button>
+      <h1>{name}</h1>
+      <ul>
+        <li>{email}</li>
+        <li>{phone}</li>
+        <li>{schoolName}</li>
+        <li>{schoolTitle}</li>
+        <li>{schoolYear}</li>
+        <li>{jobName}</li>
+        <li>{jobTitle}</li>
+        <li>{jobResponsibility}</li>
+        <li>{jobStartDate.toLocaleString().split(',')[0]}</li>
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/cvForm.jsx
+++ src/components/cvForm.jsx
@@ -1,62 +1,102 @@
-import './cvForm.css'
+import "./cvForm.css";
 
-export default function ModalForm({ onClose, dialogOpen  }) {
+export default function ModalForm({
+  onClose,
+  dialogOpen,
+  handleName,
+  handleEmail,
+  handlePhone,
+  handleSchoolName,
+  handleSchoolTitle,
+  handleSchoolYear,
+  handleJobName,
+  handleJobTitle,
+  handleJobRespo,
+  handleJobStartDate,
+}) {
+  function submitHelper() {
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const phone = document.querySelector('#phone').value;
+    const schoolName = document.querySelector('#schoolName').value;
+    const schoolTitle = document.querySelector('#schoolTitle').value;
+    const schoolYear = document.querySelector('#schoolYear').value;
+    const jobName = document.querySelector('#jobName').value;
+    const jobTitle = document.querySelector('#jobTitle').value;
+    const jobRespo = document.querySelector('#jobRespo').value;
+    const jobStartDate = document.querySelector('#jobStartDate').value;
+
+    handleName(name);
+    handleEmail(email);
+    handlePhone(phone);
+    handleSchoolName(schoolName);
+    handleSchoolTitle(schoolTitle);
+    handleSchoolYear(schoolYear);
+    handleJobName(jobName);
+    handleJobTitle(jobTitle);
+    handleJobRespo(jobRespo);
+    handleJobStartDate(jobStartDate);
+    onClose();
+  }
+
   if (!dialogOpen) return null;
 
   return (
-      <div className="formContainer">
-        <form action='' method='get' className='cvForm'>
-            <fieldset>
-              <legend>Personal Information</legend>
-              <div className='formInput'>
-                <label htmlFor="name">Name: </label>
-                <input type='text' name='name' id='name' />
-              </div>
-              <div className="formInput">
-                <label htmlFor="email">Email: </label>
-                <input type="email" name="email" id="email" />
-              </div>
-              <div className="formInput">
-                <label htmlFor="phone">Phone: </label>
-                <input type="tel" name="phone" id="phone" />
-              </div>
-            </fieldset>
-            <fieldset>
-              <legend>Educational Experience</legend>
-              <div className="formInput">
-                <label htmlFor="schoolName">School name: </label>
-                <input type="text" name="schoolName" id="schoolName" />
-              </div>
-              <div className="formInput">
-                <label htmlFor="schoolTitle">Title of Study: </label>
-                <input type="text" name="schoolTitle" id="schoolTitle" />
-              </div>
-              <div className="formInput">
-                <label htmlFor="schoolYear">Year od Study: </label>
-                <input type="text" name="schoolYear" id="schoolYear" />
-              </div>
-            </fieldset>
-            <fieldset>
-              <legend>Practical Experience</legend>
-              <div className="formInput">
-                <label htmlFor="jobName">Company Name: </label>
-                <input type="text" name="jobName" id="jobName" />
-              </div>
-              <div className="formInput">
-                <label htmlFor="jobTitle">Position Title: </label>
-                <input type="text" name="jobTitle" id="jobTitle" />
-              </div>
-              <div className="formInput">
-                <label htmlFor="jobRespo">Main responsibilities: </label>
-                <input type="text" name="jobRespo" id="jobRespo" />
-              </div>
-              <div className="formInput">
-                <label htmlFor="jobStartDate">Job Start: </label>
-                <input type="date" name="jobStartDate" id="jobStartDate" />
-              </div>
-            </fieldset>
-            <button type="button" onClick={onClose}>Submit</button>
-          </form>
-      </div>
-  )
+    <div className="formContainer">
+      <form action="" method="get" className="cvForm">
+        <fieldset>
+          <legend>Personal Information</legend>
+          <div className="formInput">
+            <label htmlFor="name">Name: </label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className="formInput">
+            <label htmlFor="email">Email: </label>
+            <input type="email" name="email" id="email" />
+          </div>
+          <div className="formInput">
+            <label htmlFor="phone">Phone: </label>
+            <input type="tel" name="phone" id="phone" />
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>Educational Experience</legend>
+          <div className="formInput">
+            <label htmlFor="schoolName">School name: </label>
+            <input type="text" name="schoolName" id="schoolName" />
+          </div>
+          <div className="formInput">
+            <label htmlFor="schoolTitle">Title of Study: </label>
+            <input type="text" name="schoolTitle" id="schoolTitle" />
+          </div>
+          <div className="formInput">
+            <label htmlFor="schoolYear">Year od Study: </label>
+            <input type="text" name="schoolYear" id="schoolYear" />
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>Practical Experience</legend>
+          <div className="formInput">
+            <label htmlFor="jobName">Company Name: </label>
+            <input type="text" name="jobName" id="jobName" />
+          </div>
+          <div className="formInput">
+            <label htmlFor="jobTitle">Position Title: </label>
+            <input type="text" name="jobTitle" id="jobTitle" />
+          </div>
+          <div className="formInput">
+            <label htmlFor="jobRespo">Main responsibilities: </label>
+            <input type="text" name="jobRespo" id="jobRespo" />
+          </div>
+          <div className="formInput">
+            <label htmlFor="jobStartDate">Job Start: </label>
+            <input type="date" name="jobStartDate" id="jobStartDate" />
+          </div>
+        </fieldset>
+        <button type="button" onClick={submitHelper}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
